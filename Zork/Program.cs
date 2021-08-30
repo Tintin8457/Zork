428c5ac296ee06e2fdf8d393feea7975a563c9f7
Brandon Perez <Bloo7885@hotmail.com>
2021-08-30 14:54:55 -0400

    Zork 1.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,6 +7,25 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            //Take in commands and identify them as uppercase
+            string inputString = Console.ReadLine().ToUpper();
+
+            //Check for look and quit commands
+            if (inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            }
+
+            else
+            {
+                Console.WriteLine("Unrecognized command.");
+            }
         }
     }
 }
